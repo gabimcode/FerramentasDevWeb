6e1fe318a3c38a3591377b0a475563225ef94feb
gabimcode <gabriellembarros1@gmail.com>
2022-09-04 00:02:43 -0300

    alteraçõesConcluidas

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-
+#importando as bibliotecas necessárias
 from flask import Flask, render_template, request, redirect, flash
 from flask_mail import Mail, Message
 from dotenv import load_dotenv
 import os
 load_dotenv()
 
-
+#criando o método de envio de emails
 app = Flask(__name__)
 app.secret_key = 'gabimc'
 
@@ -16,24 +16,24 @@ mail_settings = {
     "MAIL_USE_SSL": True,
     "MAIL_USERNAME": os.getenv("EMAIL"),
     "MAIL_PASSWORD": os.getenv("SENHA")
-    #o email e a senha estarão em um outro arquivo, o config, de onde o importamos para que não fique aparecendo na tela do app.py
+    #o email e a senha estarão em um outro arquivo, de onde o importamos para que não fique aparecendo no código
 }
 
 app.config.update(mail_settings)
 
 mail = Mail(app)
-
+#criação da class contato
 class Contato:
     def __init__(self, nome, email, mensagem):
         self.nome = nome
         self.email = email
         self.mensagem = mensagem
-
+#rota do app
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
+#captura os dados que a pessoa inseriu no portfolio e reproduz uma mensagem nos emails cadastrados abaixo
 @app.route('/send', methods=('GET', 'POST'))
 def send():
     if request.method == 'POST':
@@ -57,14 +57,11 @@ def send():
 
             '''
         )
-
+#quando o email for enviado com sucesso, colocar essa mensagem na tela
         mail.send(msg)
         flash('Mensagem enviada!')
     return redirect('/')
 
-
-
-
-
+#usando para conseguir acessar o host local sem precisar voltar ao vscode toda hora para executar o arquivo
 if __name__ == '__main__':
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,8 +1,9 @@
+//animação do código
 const item = document.querySelectorAll("[data-anime]");
-
 const animeScroll = () => {
+    // a partir de 0.85 a tela pode fazer animação
     const windowTop = window.pageYOffset + window.innerHeight * 0.85 ;
-
+// para cada elemento adiciona e remove a animação
     item.forEach((element) => {
         if (windowTop > element.offsetTop) {
             element.classList.add("animate");
@@ -18,6 +19,7 @@ window.addEventListener("scroll", ()=>{
     animeScroll();
 })
 
+// animação do botão enviar e do botão enviando
 const btnEnviar = document.querySelector('#btn-enviar')
 const btnEnviarLoader = document.querySelector('#btn-enviar-loader')
 
@@ -26,7 +28,7 @@ btnEnviar.addEventListener("click",()=>{
     btnEnviar.style.display = "none"
 
 })
-
+//tempo que o alerta de mensagem enviada fica na tela
 setTimeout(()=> {
     document.querySelector('#alerta').style.display = "none"
 }, 4000) //em milissegundo
